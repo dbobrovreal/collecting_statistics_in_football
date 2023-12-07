26f4fb24b0f00845629f6ee1747bab28afe42858
dbobrovreal <dbobrovreal@gmail.com>
2023-12-07 17:19:36 +0700

    Добавлено анатация типов и подробное описание методов

--- data_collection.py
+++ data_collection.py
@@ -30,13 +30,13 @@ def decoding_words(word) -> str:
 
 def getting_detailed_statistics(
     statistic: Dict[str, int],
-    game_status: str,
-    missed_balls: int
+    missed_balls: int,
+    minutes_missed: list
 ) -> Dict[str, int]:
     """
     Метод для получения точной статистики игроков
+    :param minutes_missed: list - Время когда команда пропустила мяч
     :param missed_balls: int - пропущенные мячи
-    :param game_status: str - статус игры
     :param statistic:
     :return: dict
     """
@@ -53,7 +53,12 @@ def getting_detailed_statistics(
     shots: int = shot_on + shot_off + shot_blocked
     gols: int = statistic.get("goals", 0)
     minute: int = statistic.get('minutesPlayed', 0)
-    clean_sheets: int = 1 if game_status == 'win' and missed_balls == 0 and minute >= 60 else 0
+    if minute >= 60 and missed_balls == 0:
+        clean_sheets = 1
+    elif 60 <= minute < minutes_missed[0]:
+        clean_sheets = 1
+    else:
+        clean_sheets = 0
 
     processed_statistics: Dict[str, int] = {
         "saves": saves,
@@ -137,25 +142,25 @@ def fill_data_player(
 ) -> tuple[Dict[str, Dict[str, DataPlayer]], List[Dict[str, str]]]:
     """
     Метод формирует точные данные об игроках
-    :param data_club_players:
+    :param data_club_players: dict -
     :param info_by_table:
     :param row:
     :param statistic:
     :return: dict
     """
     squad_list = list()
-    list_positions = ['GK', 'DEF', 'ATT DEF', 'CDM', 'CAM', 'WIN', 'FWD']
+    list_positions: list = ['GK', 'DEF', 'ATT DEF', 'CDM', 'CAM', 'WIN', 'FWD']
 
     for column in range(0, 7):
         position: str = list_positions[column]
-        if re.search('\([A-Z]{3}\)', info_by_table.iat[row, column]):
+        if re.search('\([A-Z]{3}\)', f'{info_by_table.iat[row, column]}'):
             club: str = re.search('\([A-Z]{3}\)', info_by_table.iat[row, column]).group()[1:-1]
         else:
             squad_list.append({})
             continue
-        split_name_player = info_by_table.iat[row, column].split(":")
-        split_name_player = split_name_player[1] if len(split_name_player) > 1 else split_name_player[0]
-        name = re.split('\(', split_name_player.split()[0])[0]
+        split_name_player: list = info_by_table.iat[row, column].split(":")
+        split_name_player: str = split_name_player[1] if len(split_name_player) > 1 else split_name_player[0]
+        name: str = re.split('\(', split_name_player.split()[0])[0]
         name: str = decoding_words(re.sub(r'(?<!-)(?=[A-Z])', r' \g<0>', name))
         squad_list.append({
             "name": name,
@@ -167,19 +172,21 @@ def fill_data_player(
                 if static.get(club):
                     for club_player_statistics in static[club]:
                         search_name: str = decoding_words(club_player_statistics['name'])
-                        name_separation = name.split()
+                        name_separation: list = name.split()
                         if name_similarity_check(name_from_plate=name_separation, name_from_site=search_name):
-                            stats = getting_detailed_statistics(club_player_statistics.get('statistics'),
-                                                                game_status=static.get('result_match'),
-                                                                missed_balls=static.get('goals_conceded'))
-                            data_club_players[name] = {
+                            stats: StatisticPlayer = getting_detailed_statistics(
+                                statistic=club_player_statistics.get('statistics'),
+                                missed_balls=static.get('goals_conceded'),
+                                minutes_missed=static.get('minutes_missed')
+                            )
+                            data_club_players[name]: DataPlayer = {
                                 "position": position,
                                 "club": club,
                                 'statistic': stats
                             }
                             break
                     else:
-                        data_club_players[name] = {
+                        data_club_players[name]: DataPlayer = {
                             "position": position,
                             "club": club,
                             'statistic': {
